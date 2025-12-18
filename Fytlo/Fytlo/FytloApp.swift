ee2c620a689d71ceec420bbeeb62d17951de20a4
Victor A <vicjrandu@gmail.com>
2025-12-17 19:33:40 -0500

    app to repo
    added app to repo

---
+++ Fytlo/Fytlo/FytloApp.swift
@@ -0,0 +1,17 @@
+//
+//  FytloApp.swift
+//  Fytlo
+//
+//  Created by Victor Jr on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct FytloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fytlo/FytloTests/FytloTests.swift
@@ -0,0 +1,17 @@
+//
+//  FytloTests.swift
+//  FytloTests
+//
+//  Created by Victor Jr on 12/17/25.
+//
+
+import Testing
+@testable import Fytlo
+
+struct FytloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
